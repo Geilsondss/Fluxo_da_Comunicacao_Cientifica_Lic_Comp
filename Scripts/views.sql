069b3e2abf530dad0642b50043fa735aba950f53
Geilson Sá <geilsondossantos098@gmail.com>
2025-07-19 17:43:58 -0300

    Criação da view "Instuicoes_validas"
    Criação da view "Instuicoes_validas" após orientações do professor Jonathan, nesta view é avaliado apenas as instituições com os nome dos cursos "Computação", "Ciência da Computação" e "Ciências da Computação" com data de criação ou de inicio de funcionalidade inferiores a 01-01-2020.

--- Scripts/views.sql
+++ Scripts/views.sql
@@ -1,7 +1,7 @@
-create view instituicoes_validas as
-select *
-from instituicao
-where nome_curso in ('COMPUTAÇÃO','CIÊNCIA DA COMPUTAÇÃO','CIÊNCIAS DA COMPUTAÇÃO')
-and criacao_curso < '2020-01-01'
-and inicio_funcionamento_curso < '2020-01-01';
+CREATE VIEW instituicoes_validas AS
+SELECT *
+FROM instituicao
+WHERE nome_curso IN ('COMPUTAÇÃO','CIÊNCIA DA COMPUTAÇÃO','CIÊNCIAS DA COMPUTAÇÃO')
+AND criacao_curso < '2020-01-01'
+AND inicio_funcionamento_curso < '2020-01-01';
 
