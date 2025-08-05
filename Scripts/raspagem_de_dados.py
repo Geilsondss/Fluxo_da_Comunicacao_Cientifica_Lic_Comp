7c4bdb60efd87dd7e6cc34dc02a366e05c18ed52
Geilson Sá <geilsondossantos098@gmail.com>
2025-08-05 20:30:09 -0300

    Código especificamente para o repositorio da UNIVERSIDADE FEDERAL DE SANTA MARIA

--- Scripts/raspagem_de_dados.py
+++ Scripts/raspagem_de_dados.py
@@ -1,5 +1,4 @@
 #Código especificamente para o repositorio da Universidade Estadual do Ceará
-
 TEXTO = []
 AUTOR = []
 ASSUNTO = []
@@ -14,24 +13,56 @@ for i in TEXTO:
         ativo = False
     elif autor == 'Autor:':
         AUTOR.append(i[6:])
-        
     elif assunto == 'Assuntos:':
         ASSUNTO.append(i[9:])
         ativo = True
 
 print('TITULO')
-for j in TITULO:
-    print(j)
-
+for W in TITULO:
+    print(W)
 print('')
 
 print('AUTOR')
 for j in AUTOR:
     print(j)
-    
 print('')
+
 print('ASSUNTO')
 for h in ASSUNTO:
     print(h)
 
+
+#Código especificamente para o repositorio da UNIVERSIDADE FEDERAL DE SANTA MARIA
+TEXTO = []
+AUTOR = []
+DATA = []
+TITULO = []
+CONT = 0
+
+for i in TEXTO:
+    if CONT == 0:
+        TITULO.append(i)
+        CONT = 1
+    elif CONT == 1:
+        PARENTESE = i.rfind('(')
+        AUTOR.append(i[:PARENTESE])
+        DATA.append(i[PARENTESE+1:-1])
+        CONT = 2
+    elif CONT == 2:
+        CONT = 0
+
+print('TITULO')
+for W in TITULO:
+    print(W)
+print('')
+
+print('AUTOR')
+for j in AUTOR:
+    print(j)
+print('')
+
+print('DATA')
+for h in DATA:
+    print(h)
         
+
